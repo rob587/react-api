b4433c4a8288179c83e5a8421bd049d34f8fd14b
rob587 <robertocamm02@yahoo.com>
2025-09-02 14:11:06 +0200

    axios aggiunto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const App = () => {
+
+ const [actors, setActors] = ([])
+
+ const fetchActors = () => {
+  axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) =>{
+    console.log(fetchActors)
+    setActors(resp.data)
+  })
+ }
+
   return (
-    <div>
-      
-    </div>
+    
+      <h1>ciao</h1>
+
   )
 }
 
