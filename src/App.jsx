990787d2cb2d9190b97468c5dc039b198bed01d9
rob587 <robertocamm02@yahoo.com>
2025-09-02 14:40:54 +0200

    card statica creata

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react'
+import React, { act, use } from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
@@ -7,8 +7,9 @@ const App = () => {
  const [actors, setActors] = useState([])
 
  const fetchActors = () => {
-  axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) =>{
-    console.log(resp)
+  axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) =>{
+    setActors(resp.data.results)
+   
   })
  }
 
@@ -16,7 +17,37 @@ const App = () => {
 
   return (
     
-      <h1>ciao</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className='text-center mt-5'>Attori formidabili incredibili</h1>
+          </div>
+
+            {actors.map(act => {
+              return (
+                
+              <div className="col-6" key={act.id}>
+               <div className="character">
+                  <img src={act.image} alt={act.name} />
+                  
+                  <div className="actor-text">
+                    <h3>{act.name}</h3>
+                  </div>
+
+                  <div className="infos">
+                    <p>{act.nationality}</p>
+                    <p>{act.birth_year}</p>
+                    <p>{act.biography}</p>
+                    <p>{act.known_for}</p>
+                  </div>
+
+
+                </div>
+              </div>              
+              )
+            })}
+        </div>
+      </div>
 
   )
 }
